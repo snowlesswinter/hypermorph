d0763b738ac9cfa99570cdc07d329c68e2266ef2
tanjianwen <jianwen.tan@gmail.com>
2016-06-20 18:29:42 +0800

    added copyright statement

--- source/cuda/volume_reduction.cuh
+++ source/cuda/volume_reduction.cuh
@@ -1,3 +1,22 @@
+/*
+* Copyright 1993-2015 NVIDIA Corporation.  All rights reserved.
+*
+* Please refer to the NVIDIA end user license agreement (EULA) associated
+* with this source code for terms and conditions that govern your use of
+* this software. Any use, reproduction, disclosure, or distribution of
+* this software and related documentation outside the terms of the EULA
+* is strictly prohibited.
+*
+*/
+
+/*
+Parallel reduction kernels
+*/
+
+// 
+// A customized version for volume reduction by JINWEN TAN(jianwen.tan@gmail.com).
+//
+
 #ifndef _VOLUME_REDUCTION_H_
 #define _VOLUME_REDUCTION_H_
 
--- source/poisson_solver/full_multigrid_poisson_solver.h
+++ source/poisson_solver/full_multigrid_poisson_solver.h
@@ -1,3 +1,24 @@
+//
+// Fluid3d - Fluid Simulator for interactive applications
+// Copyright (C) 2016. JIANWEN TAN(jianwen.tan@gmail.com). All rights reserved.
+//
+// Fluid3d license (* see part 1 below)
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. Acknowledgement of the
+//    original author is required if you publish this in a paper, or use it
+//    in a product.
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
 #ifndef _FULL_MULTIGRID_POISSON_SOLVER_H_
 #define _FULL_MULTIGRID_POISSON_SOLVER_H_
 
